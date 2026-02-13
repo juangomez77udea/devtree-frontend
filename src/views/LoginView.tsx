d8250bba3fbb32fbe7af8d1f0e23c2e188110917
Juan Carlos Gomez Jaramillo <juangomez88@gmail.com>
2026-02-13 15:50:02 -0500

    Commit inicial creacion de LoginView y ResgisterView

---
+++ src/views/LoginView.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+
+export default function LoginView() {
+      return (
+            <>
+            <h1 className=' text-4xl text-white font-bold'>Iniciar Sesión</h1>
+                  <nav className=' mt-10'>
+                        <Link 
+                              className=' text-center text-white text-lg font-bold'
+                              to='/auth/register'
+                        >
+                              ¿No tienes cuenta? Crea una aquí
+                        </Link>
+                  </nav>
+            </>
+      )
+}
