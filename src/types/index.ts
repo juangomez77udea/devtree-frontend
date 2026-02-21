11e8adfdda0f0f0c75577c44f46b9b2610b07fc9
Juan Carlos Gomez Jaramillo <juangomez88@gmail.com>
2026-02-21 13:03:53 -0500

    Cragar imagenes

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,6 +5,7 @@ export type User = {
       password: string
       _id: string
       description: string
+      image: string
 }
 
 export type ResgisterForm = Pick<User, "handle" | "name" | "email"> & {
--- src/views/ProfileView.tsx
+++ src/views/ProfileView.tsx
@@ -3,7 +3,7 @@ import { useQueryClient, useMutation } from '@tanstack/react-query'
 import { toast } from 'sonner'
 import ErrorMessage from '../components/ErrorMessage'
 import type { ProfileForm, User } from '../types'
-import { updateProfile } from '../api/DevTreeAPI'
+import { updateProfile, uploadImage } from '../api/DevTreeAPI'
 
 export default function ProfileView() {
       const queryClient = useQueryClient()
@@ -26,6 +26,28 @@ export default function ProfileView() {
                   queryClient.invalidateQueries({queryKey: ['user']})
             }
       })
+
+      const uploadImageMutation = useMutation({
+            mutationFn: uploadImage,
+            onError: (error) => {
+                  toast.error(error.message)
+            },
+            onSuccess: (data) => {
+                  queryClient.setQueryData(['user'], (prevData: User) => {
+                        console.log(prevData)
+                        return {
+                              ...prevData,
+                              image: data
+                        }
+                  })
+            }
+      })
+
+      const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+            if (e.target.files) {
+                  uploadImageMutation.mutate(e.target.files[0])
+            }
+      }
 
       const handleUserProfileForm = (formData: ProfileForm) => {
             updateProfileMutation.mutate(formData)
@@ -78,7 +100,7 @@ export default function ProfileView() {
                               name="handle"
                               className="border-none bg-slate-100 rounded-lg p-2"
                               accept="image/*"
-                              onChange={() => { }}
+                              onChange={handleChange}
                         />
                   </div>
 
